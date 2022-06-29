feb750d85478139ce42c21ffdbed22ae6eea2c3d
idomyshev <idomyshev@braintch.com>
2022-06-29 12:30:13 +0400

    app name changed

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ router.afterEach((to) => {
   // Use next tick to handle router history correctly
   // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
   Vue.nextTick(() => {
-    document.title = to.meta.title || "EmptyVue2App";
+    document.title = to.meta.title || "AmChartSandbox";
   });
 });
 
