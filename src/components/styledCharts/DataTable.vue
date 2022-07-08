ea3303b50393caa715804d84e5ffc1a425163c26
artem_chl <achuluhadze@braintch.com>
2022-07-08 07:18:14 +0300

    lint fix

---
+++ src/components/styledCharts/DataTable.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+import { tableData } from "@/components/styledCharts/mockData";
+
+export default {
+  name: "DataTable",
+}
+</script>
+
+<style>
+
+</style>
