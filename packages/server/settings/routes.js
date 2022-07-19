cb08b5b10852c427a33e851c1c09c5fbb2dd442d
idomyshev <idomyshev@braintch.com>
2022-07-19 13:01:10 +0400

    fix routes

--- packages/server/settings/routes.js
+++ packages/server/settings/routes.js
@@ -4,6 +4,11 @@ export const ROUTES = {
   AUTH_WITH_ONE_TIME_TOKEN: '/auth-with-one-time-token',
   CHART: '/chart',
   CHARTS: '/charts',
+  DEMO: '/demo',
+  SANDBOX: {
+    PIE: '/sandbox/pie',
+    COLUMN: '/sandbox/column'
+  }
 }
 
 export const ROUTES_WITHOUT_AUTHORIZATION = [
@@ -12,5 +17,7 @@ export const ROUTES_WITHOUT_AUTHORIZATION = [
   ROUTES.AUTH_WITH_ONE_TIME_TOKEN,
   ROUTES.CHART,
   ROUTES.CHARTS,
-  '/sandbox/pie'
+  ROUTES.SANDBOX.PIE,
+  ROUTES.SANDBOX.COLUMN,
+  ROUTES.DEMO
 ]
