044a85b3ba1ec4e60ed743de8501ff48ea5af464
idomyshev <idomyshev@braintch.com>
2022-07-26 09:40:10 +0400

    header moved

--- packages/client/src/views/GlobalOverview.vue
+++ packages/client/src/views/GlobalOverview.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="app-wrapper">
-    <Menu></Menu>
+    <Menu />
     <div class="app-content">
       <div class="global-overview">
-        <Header />
+        <div class="header">
+          <h1>Global Overview</h1>
+          <v-btn
+            :to="ROUTES.DEMO_CHARTS"
+            class="header__btn"
+            depressed
+            color="secondary"
+            >Edit Layout</v-btn
+          >
+        </div>
         <Filters />
         <Grid />
       </div>
@@ -15,20 +24,27 @@
 import Vue from "vue";
 import Filters from "@/components/Filters.vue";
 import Grid from "@/components/Grid.vue";
-import Header from "@/components/Header.vue";
 import Menu from "@/components/Menu";
+import { ROUTES } from "@/settings/routes";
 
 export default Vue.extend({
   name: "GlobalOverview",
   components: {
     Filters,
     Grid,
-    Header,
     Menu,
+  },
+  data() {
+    return {
+      ROUTES,
+    };
   },
 });
 </script>
 <style lang="scss">
+@import "./src/scss/theme.scss";
+@import "./src/scss/custom_variables.scss";
+
 .app-wrapper {
   display: flex;
 }
@@ -36,5 +52,16 @@ export default Vue.extend({
 .app-content {
   width: 100%;
   padding: 0 48px;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px 0;
+}
+
+.header__btn {
+  border-radius: $border-radius-small;
+  color: var(--v-primary-base) !important;
 }
 </style>
